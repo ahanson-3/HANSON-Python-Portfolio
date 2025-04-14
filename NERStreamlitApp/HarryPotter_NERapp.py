2dcfa1f68eb28642bbfd453cbcbe1c9f88b1d97a
Annelise Hanson <hansonannelise@gmail.com>
2025-04-14 02:43:08 -0400

    Update HarryPotter_NERapp.py
    troubleshooting take 4: url?

--- NERStreamlitApp/HarryPotter_NERapp.py
+++ NERStreamlitApp/HarryPotter_NERapp.py
@@ -61,8 +61,8 @@ The movies follow the same storyline as the books, with the final book split int
 
 st.subheader("**Let's get started on analyzing chapter 1 of Sorcerer's Stone with NER!**")
 
-# Define the file path (adjust this path if your file is stored elsewhere)
-hp_chapter1 = ('Harry_Potter_Chapter1.txt')
+# Define the file path 
+hp_chapter1 = ('https://github.com/ahanson-3/HANSON-Python-Portfolio/blob/main/NERStreamlitApp/Harry_Potter_Chapter1.txt')
 
 # Read the file content
 with open(hp_chapter1, 'r', encoding='utf-8') as f:
